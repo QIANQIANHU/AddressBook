8e01e790cf2040f24d4d4c466b0e950255327a4f
Qianqian Hu <qianhu@pindous.com>
2018-02-09 15:46:57 -0800

    add id to model

--- Models/ContactVariable.cs
+++ Models/ContactVariable.cs
@@ -5,6 +5,17 @@ namespace AddressBook.Models
     private string _userName;
     private string _phoneNumber;
     private string _address;
+    private int _id;
+
+    private static List<ContactVariable> _contacts = new List<ContactVariable>{};
+
+    public ContactVariable(string userName, string phoneNumber, string address)
+    {
+      _userName = userName;
+      _phoneNumber = phoneNumber;
+      _address = address;
+      _id = _contacts.Count + 1;
+    }
 
     public string GetUserName()
     {
@@ -34,6 +45,10 @@ namespace AddressBook.Models
     public void SetAddress(string newAddress)
     {
       _address = newAddress;
+    }
+    public int GetId()
+    {
+      return _id;
     }
   }
 
